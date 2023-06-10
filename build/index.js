10bb7573fa906b3a1f5d86d5656a7d93c4a5d3c5
Zevi Liao <jointjm@gmail.com>
2023-06-10 16:04:17 +0800

    test read file

--- build/index.js
+++ build/index.js
@@ -1,3 +1,7 @@
 "use strict";
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+var fs = require('fs');
+var path = require("path");
+var result = fs.readFileSync(path.resolve(__dirname, "../data/example.txt"), {
+    encoding: 'utf-8',
+});
+console.log(result);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+const fs = require('fs');
+const path = require("path");
+
+const result = fs.readFileSync(
+  path.resolve(__dirname, "../data/example.txt"), {
+  encoding: 'utf-8',
+});
+console.log(result);
